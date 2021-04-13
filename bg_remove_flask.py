1485f6ff3c75055ba5c914b287b740bcf5349e16
Michael Zhao <28zhaomichael@gmail.com>
2021-04-12 20:36:25 -0700

    Update bg_remove_flask.py

--- bg_remove_flask.py
+++ bg_remove_flask.py
@@ -39,10 +39,6 @@ def remove_bg():
 
 @app.route("/uploads/<filename>")
 def uploaded_file(filename):
-  return send_from_directory("output", filename)
-
-@app.route("/inputs/<filename>")
-def inputs(filename):
   return send_from_directory("input", filename)
 
 
